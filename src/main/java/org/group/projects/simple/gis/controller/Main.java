612767048de98ff97b5f42e22f703b1ba6c1967e
ol_lx <ol_lx@icloud.com>
2019-05-01 19:08:37 +0500

    feature(templates) add search-form

--- src/main/java/org/group/projects/simple/gis/controller/Main.java
+++ src/main/java/org/group/projects/simple/gis/controller/Main.java
@@ -1,5 +1,6 @@
 package org.group.projects.simple.gis.controller;
 
+import org.group.projects.simple.gis.model.SearchRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -9,7 +10,12 @@ public class Main {
 
     @GetMapping("/")
     public ModelAndView main() {
-        ModelAndView mSearchPage = new ModelAndView("redirect:/search");
-        return mSearchPage;
+        ModelAndView mainModel = new ModelAndView();
+        mainModel.setViewName("main");
+        mainModel.addObject("searchRequest", new SearchRequest());
+
+        return mainModel;
+        //ModelAndView mSearchPage = new ModelAndView("redirect:/search");
+        //return mSearchPage;
     }
 }
